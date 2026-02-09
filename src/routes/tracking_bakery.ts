fa7b84bf6cb68b74968703ba9e5a8f6217cd2030
sompadidvirada <sompadidppp@gmail.com>
2026-02-09 09:25:50 +0700

    asd

--- src/routes/tracking_bakery.ts
+++ src/routes/tracking_bakery.ts
@@ -15,12 +15,12 @@ router.post("/uploadfiletracksell", verifyTokenAndRole(["ADMIN", "STAFF_SPC"]), 
 
 //tracking send routes
 
-router.post("/trackingsend", verifyTokenAndRole(["ADMIN", "STAFF_SPC"]), inertTracksend)
-router.post("/getbakerysend", verifyTokenAndRole(["ADMIN", "STAFF_SPC"]), getBakerySend)
-router.put("/updatetracksend/:id", verifyTokenAndRole(["ADMIN", "STAFF_SPC"]), editTrackSend)
-router.delete("/deletetracksend/:id", verifyTokenAndRole(["ADMIN", "STAFF_SPC"]), deleteTrackSend)
-router.post("/deletealltracksend", verifyTokenAndRole(["ADMIN", "STAFF_SPC"]), deleteAllTrackSend)
-router.post("/uploadtrackingsend", verifyTokenAndRole(["ADMIN", "STAFF_SPC"]), uploadTrackSend)
+router.post("/trackingsend", verifyTokenAndRole(["ADMIN", "STAFF_SPC", "STAFF_WH"]), inertTracksend)
+router.post("/getbakerysend", verifyTokenAndRole(["ADMIN", "STAFF_SPC", "STAFF_WH"]), getBakerySend)
+router.put("/updatetracksend/:id", verifyTokenAndRole(["ADMIN", "STAFF_SPC", "STAFF_WH"]), editTrackSend)
+router.delete("/deletetracksend/:id", verifyTokenAndRole(["ADMIN", "STAFF_SPC", "STAFF_WH"]), deleteTrackSend)
+router.post("/deletealltracksend", verifyTokenAndRole(["ADMIN", "STAFF_SPC", "STAFF_WH"]), deleteAllTrackSend)
+router.post("/uploadtrackingsend", verifyTokenAndRole(["ADMIN", "STAFF_SPC", "STAFF_WH"]), uploadTrackSend)
 
 //tracking exp routes
 
@@ -30,6 +30,6 @@ router.delete("/deletetrackexp/:id", verifyTokenAndRole(["ADMIN", "STAFF_SPC"]),
 router.post("/deletalltrackexp", verifyTokenAndRole(["ADMIN", "STAFF_SPC"]), deleteAllTrackExp)
 
 // get bakery available on branch
-router.post("/getavailablebakerys", verifyTokenAndRole(["ADMIN", "STAFF_SPC", "BARISTAR"]), getAvailableBakery)
+router.post("/getavailablebakerys", verifyTokenAndRole(["ADMIN", "STAFF_SPC", "BARISTAR", "STAFF_WH"]), getAvailableBakery)
 
 export default router
