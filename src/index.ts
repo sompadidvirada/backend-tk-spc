52c01937aa112b0fb35aa705046370e0d9292099
sompadidvirada <sompadidppp@gmail.com>
2026-02-09 09:03:16 +0700

    asdfasdf

--- src/index.ts
+++ src/index.ts
@@ -42,8 +42,7 @@ app.use(
     origin: [
       "https://treekoff.store",
       "https://www.treekoff.store",
-      "http://localhost:3000",
-      "http://192.168.1.19:3000"
+      "http://192.168.1.40:3000"
     ],
     credentials: true,
   }),
